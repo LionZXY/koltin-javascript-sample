776df3edc7df8797ac9b4db2b504e4fae25e571a
lionzxy <nikita@kulikof.ru>
2017-12-19 11:51:34 +0300

    Initial commit

---
+++ src/test-file.kt
@@ -0,0 +1,8 @@
+package org.test
+
+import kotlin.math.*;
+
+fun main(args: Array<String>) {
+    println(getSomething())
+    println(acos(1))
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const KotlinWebpackPlugin = require('kotlin-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: 'kotlinApp', // kotlinApp is the default module name
+
+    resolve: {
+        modules: ['node_modules', 'kotlin_build']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, '../kotlin_build'),
+                use: ['source-map-loader'],
+                enforce: 'pre',
+            },
+        ],
+    },
+
+    output: {
+        path: __dirname + '/build',
+        filename: 'build.js',
+    },
+
+    plugins: [
+        new KotlinWebpackPlugin({
+            src: __dirname + '/src',
+        })
+    ]
+};
